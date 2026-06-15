31b12ce799709b5d34ab2f50e15302d2a7a08c68
EliaukoaYoW <2746548120@qq.com>
2026-06-15 17:46:04 +0800

    [fix] minimal error

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
-from pico.evalutor import run_harness_regression_v2
-from pico.metrics import (
+from codini.evalutor import run_harness_regression_v2
+from codini.metrics import (
     run_memory_ablation_v2, 
     run_context_ablation_v2,
     run_recovery_ablation_v2,
