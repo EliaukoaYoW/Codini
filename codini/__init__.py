142e667e0e9a523cfbb9a307cbfd36ae60b281fa
EliaukoaYoW <2746548120@qq.com>
2026-07-10 18:08:16 +0800

    [add] ...

--- codini/__init__.py
+++ codini/__init__.py
@@ -1,11 +1,13 @@
 from .cli import build_agent, build_arg_parser, build_welcome, main
 from .models import FakeModelClient, OllamaModelClient, OpenAICompatibleModelClient,SiliconflowModelClient
 from .runtime import MiniAgent, Codini, SessionStore
+from .run_store import RunStore
 from .workspace import WorkspaceContext
 
 __all__ = [
     "FakeModelClient",
     "Codini",
+    "RunStore",
     "build_agent",
     "build_arg_parser",
     "build_welcome",
